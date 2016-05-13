0d9316928ef228b6622b105a62297b00c750cef5
abdulsipit <abdulsipit007@gmail.com>
2016-05-13 16:34:52 +0700

    penambahan judul

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 
 public class main {
 	public static void main(String[] args) {
-		
+		System.out.println("Ujian RKPPL 133040312");
 		for (int i = 0; i < 5; i++) {
 			int nilai_awal = i;
 			int hasil = nilai_awal + 1;
