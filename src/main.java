e82e01f918f185ec6bcbea60eeb761928d200f0e
sukanda237 <sukanda.133040237@mail.unpas.ac.id>
2016-05-13 16:20:20 +0700

    commite pertama

---
+++ src/main.java
@@ -0,0 +1,11 @@
+
+public class main {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 5; i++) {
+			int nilai_awal = i;
+			int hasil = nilai_awal + 1;
+			System.out.println("nilai [i] = "+hasil);
+		}
+	}
+}
